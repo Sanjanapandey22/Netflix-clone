64b8981a521304a45f4bd67c5de5c54bf463e5d2
Sanjup22 <sanjanapandey1979@gmail.com>
2025-07-24 23:44:32 +0530

    ...

--- Netflix-clone/src/pages/Player/Player.jsx
+++ Netflix-clone/src/pages/Player/Player.jsx
@@ -33,15 +33,12 @@ const options = {
 
   }, [])
 
-const trailer = apiData.results.find(
-  (video) => video.type === "Trailer" && video.site === "YouTube"
-);
 
 
     return(
         <div className="player">
             <img src="/back_arrow_icon.png" alt="" onClick={()=>{navigate(-2)}} />
-            <iframe width="90%" height="90%"   src={`https://www.youtube.com/embed/${trailer?.key}`} 
+            <iframe width="90%" height="90%"   src={`https://www.youtube.com/embed/${apiData.key}`} 
 
             title="trailer" frameBorder='0' allowFullScreen></iframe>
             <div className="player-info">
